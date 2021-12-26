15ae3893a0beeb5ac4f2cbf78d73615fa1a6598e
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-26 22:38:25 +0330

    Add files via upload

---
+++ (While Loop)with reapeat in input.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<cmath>
+#include <conio.h>
+
+using namespace std;
+int main()
+
+{
+	double a;
+	
+	cout<<"Enter A Positive Number:"<<endl;
+	cin>>a;
+	
+	while (0 < a)
+	
+	{
+		cout<<"sqrt ("<<a<<") = "<<sqrt(a)<<endl;
+		
+		cout<<"Enter Another Positive Number Or (0 To Quit)"<<endl;
+			
+		cin>>a;
+	}
+	
+	getch();
+
+}
